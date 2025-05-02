7724d263d3091e354c0f9bda69e77c42b9d791df
AHMED RADWAN <mhaahmed710@gmail.com>
2025-05-03 02:40:44 +0300

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,18 +1,10 @@
 const js = require("@eslint/js");
-const json = require("@eslint/json");
-const pluginReact = require("eslint-plugin-react");
 const { defineConfig } = require("eslint/config");
 
 module.exports = defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
-    extends: ["js/recommended"]
-  },
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"]
+    extends: ["js:recommended"]
   }
 ]);
